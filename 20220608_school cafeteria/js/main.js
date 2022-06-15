7ebb54a5ccce20e6c4a1ef1c3606047617ca50fa
soyoung <soyeongan4@gmail.com>
2022-06-15 12:00:47 +0900

    js change toggle icon

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -3,14 +3,17 @@ const toggleMenu =  (toggleId,navListId) => {
 //html->js
 const toggle =document.getElementById(toggleId); 
 const navList =document.getElementById(navListId);
+const toggleIcon = toggle.getElementsByTagName("i")[0];
 
  if(toggle && navList){
     //toggle click
     toggle.addEventListener('click',() => {
          //show/hide menu : .show-menu
         navList.classList.toggle('show-menu');
-    });
-      
+        //change toggle icon:bx-menu <-> bx-x
+        toggleIcon.classList.toggle("bx-menu");
+        toggleIcon.classList.toggle("bxs-x-square");
+    });   
  }
 }
 toggleMenu("nav-toggle","nav-list");
